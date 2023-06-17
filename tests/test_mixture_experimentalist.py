829656d7d137ad3ed21600823f0dd47662fa31af
blinodelka <45514183+blinodelka@users.noreply.github.com>
2023-06-17 12:48:46 +0400

    Added tests

--- tests/test_mixture_experimentalist.py
+++ tests/test_mixture_experimentalist.py
@@ -1,13 +1,36 @@
-from autora.experimentalist.sampler.mixture_experimentalist import example_sampler
+from autora.experimentalist.sampler.mixture_experimentalist import mixture_sampler
 import numpy as np
+import pytest
 
-def test_output_dimensions():
-    X = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
-    n = 2
-    X_new = example_sampler(X, n)
+def mock_sampler(condition_pool, **kwargs):
+    return condition_pool, np.random.rand(len(condition_pool))
 
-    # Check that the sampler returns n experiment conditions
-    assert X_new.shape == (n, X.shape[1])
+def test_mixture_sampler():
+    condition_pool = np.array([1, 2, 3, 4, 5])
+    temperature = 0.5
+    samplers = [[mock_sampler, "mock", 1]]
+    params = {"mock": {}}
 
+    # Test that the function runs without errors
+    conditions = mixture_sampler(condition_pool, temperature, samplers, params)
+    assert conditions is not None
 
-# Note: We encourage you to adjust this test and write more tests.
+    # Test that the function returns the correct number of samples
+    conditions = mixture_sampler(condition_pool, temperature, samplers, params, num_samples=2)
+    assert len(conditions) == 2
+
+    # Test that the function returns unique samples when replace=False
+    conditions = mixture_sampler(condition_pool, temperature, samplers, params, num_samples=len(condition_pool))
+    assert len(conditions) == len(set(conditions))
+
+    # Test that the function raises an error when temperature is 0
+    with pytest.raises(AssertionError):
+        conditions = mixture_sampler(condition_pool, 0, samplers, params)
+
+    # Test that the function raises an error when num_samples is greater than the size of the condition pool
+    with pytest.raises(ValueError):
+        conditions = mixture_sampler(condition_pool, temperature, samplers, params, num_samples=len(condition_pool) + 1)
+
+    # Test that the function raises an error when a sampler is not provided in the samplers list
+    with pytest.raises(KeyError):
+        conditions = mixture_sampler(condition_pool, temperature, [[mock_sampler, "nonexistent", 1]], params)
